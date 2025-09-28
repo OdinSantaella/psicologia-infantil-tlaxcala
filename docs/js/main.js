6e84b9e64610cc5d7e5ab11fe742ca7f10546e22
OdinSantaella <odinsantaella@gmail.com>
2025-09-27 21:25:46 -0600

    Fixed styles and js

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,8 +1,8 @@
-import { toggleMobileMenu, initMobileMenu } from 'menu.js';
-import { initSmoothScrolling, initScrollReveal } from 'scroll.js';
-import { initHeaderScrollEffect } from 'header.js';
-import { initButtonEffects } from 'buttons.js';
-import { initPerformanceOptimizations } from 'performance.js';
+import { toggleMobileMenu, initMobileMenu } from './menu.js';
+import { initSmoothScrolling, initScrollReveal } from './scroll.js';
+import { initHeaderScrollEffect } from './header.js';
+import { initButtonEffects } from './buttons.js';
+import { initPerformanceOptimizations } from './performance.js';
 
 document.addEventListener('DOMContentLoaded', () => {
     initMobileMenu();
