b6469834089e818b2193eba1f6b6c82f6fc861ec
OdinSantaella <odinsantaella@gmail.com>
2026-06-11 13:32:46 -0600

    agregado scrollheader

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,6 +1,6 @@
 import { toggleMobileMenu, initMobileMenu } from './menu.js';
 import { initSmoothScrolling, initScrollReveal } from './scroll.js';
-import { initHeaderScrollEffect } from './header.js';
+import { initHeaderScrollEffect, initScrollHeader } from './header.js';
 import { initButtonEffects } from './buttons.js';
 import { initPerformanceOptimizations } from './performance.js';
 
